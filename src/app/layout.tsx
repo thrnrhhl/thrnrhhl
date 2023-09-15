6a18179e690eaab7595bed0eed80e6fee9ffcf2e
Pavel Yakovlev <thrnrhhl@yandex.ru>
2023-09-15 11:44:01 +0300

    Добавлена яндекс метрика

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next';
 import favicon from './favicon.ico';
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -30,6 +31,30 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&family=Unbounded:wght@200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(94955185, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+            `
+          }}
+        />
+
+        <noscript><div><img src="https://mc.yandex.ru/watch/94955185" style={{ position: 'absolute', left: -9999 }} alt="" /></div></noscript>
+        {/* /Yandex.Metrika counter */}
       </head>
       <body className="backdrop-blur-[100px] bg-[#fff] h-full">
         {children}
